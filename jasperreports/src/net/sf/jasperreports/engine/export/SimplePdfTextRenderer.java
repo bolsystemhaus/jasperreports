748dde081a9c2a3360cf7586c18afc3ebe36ed5b
bol-systemhaus <info@bol-systemhaus.de>
2024-07-29 13:11:01 +0200

    GW-648: Paragraph processing modified

--- jasperreports/src/net/sf/jasperreports/engine/export/SimplePdfTextRenderer.java
+++ jasperreports/src/net/sf/jasperreports/engine/export/SimplePdfTextRenderer.java
@@ -25,16 +25,30 @@ package net.sf.jasperreports.engine.export;
 
 import java.text.AttributedCharacterIterator;
 import java.text.AttributedString;
+import java.text.AttributedCharacterIterator.Attribute;
 
+import java.util.Locale;
+import java.util.Map;
+
+import com.lowagie.text.Chunk;
+import com.lowagie.text.Font;
+import com.lowagie.text.pdf.BaseFont;
+
+import net.sf.jasperreports.engine.JRPrintHyperlink;
 import net.sf.jasperreports.engine.JRPrintText;
+import net.sf.jasperreports.engine.JRPropertiesMap;
 import net.sf.jasperreports.engine.JasperReportsContext;
+import net.sf.jasperreports.engine.type.HyperlinkTypeEnum;
 import net.sf.jasperreports.engine.type.RunDirectionEnum;
 import net.sf.jasperreports.engine.util.JRStyledText;
+import net.sf.jasperreports.engine.util.JRTextAttribute;
 import net.sf.jasperreports.export.pdf.PdfPhrase;
 import net.sf.jasperreports.export.pdf.PdfProducer;
 import net.sf.jasperreports.export.pdf.PdfTextAlignment;
+import net.sf.jasperreports.export.pdf.PdfTextChunk;
 import net.sf.jasperreports.export.pdf.PdfTextRendererContext;
 import net.sf.jasperreports.export.pdf.TextDirection;
+import net.sf.jasperreports.export.pdf.classic.ClassicChunk;
 
 
 /**
@@ -134,76 +148,345 @@ public class SimplePdfTextRenderer extends AbstractPdfTextRenderer
 		boolean isArtifact = 
 				(text.getPropertiesMap().containsProperty("net.sf.jasperreports.export.pdf.tag.artifact") &&
 				!"none".equals(text.getPropertiesMap().getProperty("net.sf.jasperreports.export.pdf.tag.artifact")) ? true : false);	
-		tagHelper.startText(text.getLinkType() != null, isArtifact);
 
-		if (bulletChunk != null)
-		{
-			PdfPhrase phrase = pdfProducer.createPhrase();
-			pdfExporter.getPhrase(bulletChunk, bulletText, text, phrase);
+        final JRPropertiesMap jrPropertiesMap = text.getPropertiesMap();
+        final String partValue = jrPropertiesMap.getProperty("net.sf.jasperreports.export.pdf.tag.part");
 
-			phrase.go(
-				x + leftPadding,
-				yLine,
-				htmlListIndent + x + leftPadding - 10,
-				pdfExporter.getCurrentPageFormat().getPageHeight()
-					- y
-					- height
-					+ bottomPadding,
-				0,//text.getLineSpacingFactor(),// * text.getFont().getSize(),
-				text.getLineSpacingFactor(),
-				PdfTextAlignment.RIGHT,
-				TextDirection.LTR
-				);
-		}
+        final boolean part;
+        if ("true".equals(partValue)) {
+            part = true;
+        } else {
+            part = false;
+        }
 
-		bulletText = null;
-		bulletChunk = null;
+        if (part) {
+    		if (bulletChunk != null)
+    		{
+				tagHelper.startText(text.getLinkType() != null, isArtifact);
 
-		AttributedString paragraph = null;
+    			PdfPhrase phrase = pdfProducer.createPhrase();
+    			pdfExporter.getPhrase(bulletChunk, bulletText, text, phrase);
+
+    			phrase.go(
+    				x + leftPadding,
+    				yLine,
+    				htmlListIndent + x + leftPadding - 10,
+    				pdfExporter.getCurrentPageFormat().getPageHeight()
+    					- y
+    					- height
+    					+ bottomPadding,
+    				0,//text.getLineSpacingFactor(),// * text.getFont().getSize(),
+    				text.getLineSpacingFactor(),
+    				PdfTextAlignment.RIGHT,
+    				TextDirection.LTR
+    				);
+
+				tagHelper.endText();
+    		}
+
+    		bulletText = null;
+    		bulletChunk = null;
+
+    		AttributedString paragraph = null;
 		
-		if (paragraphText == null)
-		{
-			paragraphText = " ";
-			paragraph = 
-				new AttributedString(
-					paragraphText,
-					new AttributedString(
-						allParagraphs, 
-						paragraphStart, 
-						paragraphStart + paragraphText.length()
-						).getIterator().getAttributes()
-					);
-		}
-		else
-		{
-			paragraph = 
-				new AttributedString(
-					allParagraphs, 
-					paragraphStart, 
-					paragraphStart + paragraphText.length()
-					);
-		}
+    		if (paragraphText == null)
+    		{
+    			paragraphText = " ";
+    			paragraph = 
+    				new AttributedString(
+    					paragraphText,
+    					new AttributedString(
+    						allParagraphs, 
+    						paragraphStart, 
+    						paragraphStart + paragraphText.length()
+    						).getIterator().getAttributes()
+    					);
+    		}
+    		else
+    		{
+    			paragraph = 
+    				new AttributedString(
+    					allParagraphs, 
+    					paragraphStart, 
+    					paragraphStart + paragraphText.length()
+    					);
+    		}
+
+            final float llx = htmlListIndent + x + leftPadding;
+            final float urx = x + width - rightPadding;
+            final float ury = pdfExporter.getCurrentPageFormat().getPageHeight()
+                    - y
+                    - height
+                    + bottomPadding
+                    - (legacyTextMeasuringFix ? 1 : 0);
+            final float fixedLeading = 0; // text.getLineSpacingFactor(),// * text.getFont().getSize(),
+            final float multipliedLeading = text.getLineSpacingFactor();
+            final TextDirection runDirection = text.getRunDirectionValue() == RunDirectionEnum.LTR
+                    ? TextDirection.LTR : TextDirection.RTL;
+
+            final float rectangularWidth = urx - llx;
+
+            AttributedCharacterIterator iterator = paragraph.getIterator();
+            Locale locale = pdfExporter.getTextLocale(text);
+
+            int runLimit = 0;
+            float minNextLine = yLine;
+            float activeLine = yLine;
+            float leftWidth = rectangularWidth;
+            boolean firstChunk = true;
+            while(runLimit < paragraphText.length() && (runLimit = iterator.getRunLimit()) <= paragraphText.length())
+            {
+                Map<Attribute,Object> attributes = iterator.getAttributes();
+                PdfTextChunk chunk = pdfExporter.getChunk(attributes, paragraphText.substring(iterator.getIndex(), runLimit), locale);
+			
+                JRPrintHyperlink hyperlink = text;
+                if (hyperlink.getHyperlinkTypeValue() == HyperlinkTypeEnum.NONE)
+                {
+                    hyperlink = (JRPrintHyperlink)attributes.get(JRTextAttribute.HYPERLINK);
+                }
+			
+                pdfExporter.setHyperlinkInfo(chunk, hyperlink);
+
+                if (chunk instanceof ClassicChunk) {
+                    final ClassicChunk classicChunk = (ClassicChunk) chunk;
+
+                    final Chunk innerChunk = classicChunk.getChunk();
+
+                    final String content = innerChunk.getContent();
+
+                    final int contentLength = content.length();
+
+                    final Font font = innerChunk.getFont();
+
+                    final float size = font.getSize();
+
+                    final BaseFont calculatedBaseFont = font.getCalculatedBaseFont(true);
+
+                    final Map<String, Object> chunkAttributeMap = innerChunk.getChunkAttributes();
+
+                    final boolean link;
+                    if (null != chunkAttributeMap) {
+                        final Object actionObject = chunkAttributeMap.get(Chunk.ACTION);
+
+                        if (null != actionObject) {
+                            link = true;
+                        } else {
+                            link = false;
+                        }
+                    } else {
+                        link = false;
+                    }
+
+                    int previousPartIndex = 0;
+                    while (true) {
+                        if (contentLength < previousPartIndex + 1) {
+                            break;
+                        }
+
+                        boolean full = false;
+                        boolean complete = false;
+                        int lastSeparatorPartIndex = -1;
+                        int partIndex = previousPartIndex;
+                        float lastSeparatorPartWidth = 0F;
+                        float partWidth = 0F;
+                        while (true) {
+                            if (contentLength < partIndex + 1) {
+                                complete = true;
+
+                                break;
+                            }
+
+                            final int codePoint = content.codePointAt(partIndex);
+
+                            final int charCount = Character.charCount(codePoint);
+
+                            final int endPartIndex = partIndex + charCount;
+
+                            final String contentSubstring = content.substring(partIndex, endPartIndex);
+
+                            final float charWidth = calculatedBaseFont.getWidthPoint(contentSubstring, size);
+
+                            final float endPartWidth = partWidth + charWidth;
+
+                            if (rectangularWidth < endPartWidth) {
+                                break;
+                            }
+
+                            if (!full && leftWidth < endPartWidth) {
+                                if (lastSeparatorPartIndex > -1) {
+                                    break;
+                                }
+
+                                full = true;
+                            }
+
+                            final boolean whitespace = Character.isWhitespace(codePoint);
+
+                            if (whitespace) {
+                                lastSeparatorPartIndex = endPartIndex;
+                                lastSeparatorPartWidth = endPartWidth;
+                            }
+
+                            partIndex = endPartIndex;
+                            partWidth = endPartWidth;
+                        }
+
+                        final String partContent;
+                        final float resultWidth;
+                        if (complete) {
+                            partContent = content.substring(previousPartIndex, contentLength);
+
+                            resultWidth = partWidth;
+
+                            previousPartIndex = contentLength;
+                        } else if (lastSeparatorPartIndex > -1) {
+                            partContent = content.substring(previousPartIndex, lastSeparatorPartIndex);
+
+                            resultWidth = lastSeparatorPartWidth;
+
+                            previousPartIndex = lastSeparatorPartIndex;
+                        } else {
+                            partContent = content.substring(previousPartIndex, partIndex);
+
+                            resultWidth = partWidth;
+
+                            previousPartIndex = partIndex;
+                        }
+
+                        final PdfTextChunk partChunk = pdfExporter.getChunk(attributes, partContent, locale);
+
+                        if (firstChunk)
+                        {
+                            // only set anchor + bookmark for the first chunk in the text
+                            pdfExporter.setAnchor(partChunk, text, text);
+                        }
+
+                        pdfExporter.setHyperlinkInfo(partChunk, hyperlink);
+
+                        tagHelper.startText(link, isArtifact);
+
+                        final PdfPhrase partPdfPhrase = pdfProducer.createPhrase();
+
+                        partPdfPhrase.add(partChunk);
+
+                        final float partLlx;
+                        final float partLly;
+                        if (resultWidth < leftWidth) {
+                            partLlx = llx + rectangularWidth - leftWidth;
+
+                            partLly = activeLine;
+
+                            leftWidth = leftWidth - resultWidth;
+                        } else {
+                            partLlx = llx;
+
+                            partLly = minNextLine;
+
+                            activeLine = minNextLine;
+
+                            minNextLine = yLine;
+
+                            leftWidth = rectangularWidth - resultWidth;
+                        }
+
+                        final PdfTextAlignment partAlignment = PdfTextAlignment.LEFT;
+
+                        yLine = partPdfPhrase.go(
+                                partLlx,
+                                partLly,
+                                urx,
+                                ury,
+                                fixedLeading,
+                                multipliedLeading,
+                                partAlignment,
+                                runDirection);
+
+                        tagHelper.endText();
+
+                        if (yLine < minNextLine) {
+                            minNextLine = yLine;
+                        } else if (yLine > minNextLine) {
+                            yLine = minNextLine;
+                        }
+
+                        firstChunk = false;
+                    }
+                }
+
+                iterator.setIndex(runLimit);
+            }
+        } else {
+    		tagHelper.startText(text.getLinkType() != null, isArtifact);
+
+    		if (bulletChunk != null)
+    		{
+    			PdfPhrase phrase = pdfProducer.createPhrase();
+    			pdfExporter.getPhrase(bulletChunk, bulletText, text, phrase);
+
+    			phrase.go(
+    				x + leftPadding,
+    				yLine,
+    				htmlListIndent + x + leftPadding - 10,
+    				pdfExporter.getCurrentPageFormat().getPageHeight()
+    					- y
+    					- height
+    					+ bottomPadding,
+    				0,//text.getLineSpacingFactor(),// * text.getFont().getSize(),
+    				text.getLineSpacingFactor(),
+    				PdfTextAlignment.RIGHT,
+    				TextDirection.LTR
+    				);
+    		}
+
+    		bulletText = null;
+    		bulletChunk = null;
+
+    		AttributedString paragraph = null;
 		
-		PdfPhrase phrase = pdfProducer.createPhrase();
-		pdfExporter.getPhrase(paragraph, paragraphText, text, phrase);
-		yLine = phrase.go(
-			htmlListIndent + x + leftPadding,
-			yLine,
-			x + width - rightPadding,
-			pdfExporter.getCurrentPageFormat().getPageHeight()
-				- y
-				- height
-				+ bottomPadding
-				- (legacyTextMeasuringFix ? 1 : 0),
-			0,//text.getLineSpacingFactor(),// * text.getFont().getSize(),
-			text.getLineSpacingFactor(),
-			horizontalAlignment == PdfTextAlignment.JUSTIFIED && (isLastParagraph && justifyLastLine) 
-				? PdfTextAlignment.JUSTIFIED_ALL : horizontalAlignment,
-			text.getRunDirectionValue() == RunDirectionEnum.LTR
-				? TextDirection.LTR : TextDirection.RTL
-			);
+    		if (paragraphText == null)
+    		{
+    			paragraphText = " ";
+    			paragraph = 
+    				new AttributedString(
+    					paragraphText,
+    					new AttributedString(
+    						allParagraphs, 
+    						paragraphStart, 
+    						paragraphStart + paragraphText.length()
+    						).getIterator().getAttributes()
+    					);
+    		}
+    		else
+    		{
+    			paragraph = 
+    				new AttributedString(
+    					allParagraphs, 
+    					paragraphStart, 
+    					paragraphStart + paragraphText.length()
+    					);
+    		}
 		
-		tagHelper.endText();
+    		PdfPhrase phrase = pdfProducer.createPhrase();
+    		pdfExporter.getPhrase(paragraph, paragraphText, text, phrase);
+    		yLine = phrase.go(
+    			htmlListIndent + x + leftPadding,
+    			yLine,
+    			x + width - rightPadding,
+    			pdfExporter.getCurrentPageFormat().getPageHeight()
+    				- y
+    				- height
+    				+ bottomPadding
+    				- (legacyTextMeasuringFix ? 1 : 0),
+    			0,//text.getLineSpacingFactor(),// * text.getFont().getSize(),
+    			text.getLineSpacingFactor(),
+    			horizontalAlignment == PdfTextAlignment.JUSTIFIED && (isLastParagraph && justifyLastLine) 
+    				? PdfTextAlignment.JUSTIFIED_ALL : horizontalAlignment,
+    			text.getRunDirectionValue() == RunDirectionEnum.LTR
+    				? TextDirection.LTR : TextDirection.RTL
+    			);
+		
+    		tagHelper.endText();
+        }
 	}
 
 
